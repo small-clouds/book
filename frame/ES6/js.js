827c6625888ca176178ffcac70dc72a64d021a1d
15030545985 <1096250669@qq.com>
2017-11-17 16:23:19 +0800

    增加框架的分类

---
+++ frame/ES6/js.js
@@ -0,0 +1,3 @@
+﻿// 解决浏览器时间无效 不兼容问题
+var activestarttime = document.getElementById("activestarttime"); // 获取时间
+var activestarttime_date=(new Date(activestarttime.value.replace(/-/g,"/"))).getTime(); // 转换成时间戳
