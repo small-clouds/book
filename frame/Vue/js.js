1a8dcd03c4c0e49a36cc488a6a731dfafaaf5352
15030545985 <1096250669@qq.com>
2017-11-23 00:28:16 +0800

    整理完毕最终版本了吧

--- frame/Vue/js.js
+++ frame/Vue/js.js
@@ -43,3 +43,90 @@ var app7 = new Vue({
         ]
     }
 })
+var cityList = ["我是一", "我是二", "我是三", "我是四", "我是五",]
+var app8 = new Vue({
+    el: '#app8',
+    data: {
+        message: 1495855690000,
+        aaaa: '测试我容易吗',
+        cityList: cityList
+    },
+    methods: {
+        greet: function (b) {
+            // // 方法内 `this` 指向 vm
+            //alert(this.aaaa)
+            alert(b)
+            //alert(b)
+        },
+        reversedMessage: function () {
+            // `this` points to the vm instance
+            //return this.message.split('').reverse().join('')
+            var a = this.message;
+            var date = new Date(a);
+            var Y = date.getFullYear() + '-';
+            M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+            D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+            h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+            m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+            s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+            var time_new_xu = Y + M + D + h + m + s;
+            return time_new_xu;
+        }
+    },
+    computed: {
+        // a computed getter
+        reversedMessage1: function () {
+            // `this` points to the vm instance
+            //return this.message.split('').reverse().join('')
+            var a = this.message;
+            var date = new Date(a);
+            var Y = date.getFullYear() + '-';
+            M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+            D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+            h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+            m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+            s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+            var time_new_xu = Y + M + D + h + m + s;
+            return time_new_xu;
+        }
+    }
+});
+var app9 = new Vue({
+    el: '#app9',
+    data: {
+        newPerson: {
+            name: '',
+            age: 0,
+            sex: 'Male'
+        },
+        people: [{
+            name: 'Jack',
+            age: 30,
+            sex: 'Male'
+        }, {
+            name: 'Bill',
+            age: 26,
+            sex: 'Male'
+        }, {
+            name: 'Tracy',
+            age: 22,
+            sex: 'Female'
+        }, {
+            name: 'Chris',
+            age: 36,
+            sex: 'Male'
+        }]
+    },
+    methods: {
+        createPerson: function () {
+            this.people.push(this.newPerson);
+            // 添加完newPerson对象后，重置newPerson对象
+            this.newPerson = { name: '', age: 0, sex: 'Male' }
+        },
+        deletePerson: function (index) {
+            // 删一个数组元素
+            this.people.splice(index, 1);
+        }
+    }
+})
+
