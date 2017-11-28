7375a810d0fc3a64d6f57b26ddabb7be65ee8fda
15030545985 <1096250669@qq.com>
2017-11-28 12:00:29 +0800

    jquery提交

--- frame/Jquery/js.js
+++ frame/Jquery/js.js
@@ -1,1 +1,31 @@
-// alert($("a[href=hehe]").html())
+// console.log($("a[href=hehe]").html())
+$.each([1, 2, 3, 4], function (key, value) {
+    console.log(key + ":" + value)
+})
+
+$("div").each(function (e, v) {
+    console.log($(v).text())
+})
+
+$.ajax({
+    url: "hello.do",
+    type: 'GET',
+    async: true,
+    data: "username",
+    success: function () {
+        alert(data);
+    }
+})
+
+$.get(
+    "url.do", 
+    { "username": "zhangsan", "pwd": "12345" },
+    function (data) {
+        console.log(data);
+})
+$.post(
+    "url.do", 
+    { "username": "zhangsan", "pwd": "12345" },
+    function (data) {
+        console.log(data);
+})
