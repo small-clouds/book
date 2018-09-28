82a7ac4201922a18d5986a76e0cd0eaeafa3cb76
尹栋力 <1096250669@qq.com>
2018-09-28 09:22:05 +0800

    简谱+1

--- data/jianpu01.js
+++ data/jianpu01.js
@@ -43,6 +43,7 @@ var demo01 = {
         title: "F开头的简谱",
         data: [
             { context: '<a href="' + urlposition + '飞雪玉花.jpg">飞雪玉花</a>' },
+            { context: '<a href="' + urlposition + '芙蓉愿.jpg">芙蓉愿</a>' },
         ]
     }, {
         title: "G开头的简谱",
