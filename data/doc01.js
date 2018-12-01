552ba832c2a5de6f8fe6605e8053186178437eef
15030545985 <1096250669@qq.com>
2018-12-01 12:28:14 +0800

    左传整理+1  linux命令+1

--- data/doc01.js
+++ data/doc01.js
@@ -7,14 +7,26 @@ var demo01 = {
         data: [
             { context: ' 查看端口号 ---  netstat -aon|findstr 8085' },
             { context: '强制杀死某个进程  ---  taskkill -PID <进程号> -F' },
+            { context: '本机ip配置信息 ---  ipconfig' },
         ]
-    },{
+    }, {
+        title: "linux命令集合",
+        data: [
+            { context: '创建或者编辑某文件 --- vi  ' },
+            { context: '退出编辑 ---esc : exit /wq ' },
+        ]
+    }, {
         title: "git命令集合",
         data: [
             { context: '回退版本 ---   git reset --hard HEAD^ ' },
             { context: '合并dev到当前分支上 git merge dev' },
         ]
-    },{
+    }, {
+        title: "何谓之无名",
+        data: [
+            { context: '8888888517418544' },
+        ]
+    }, {
         title: "魔方公式",
         data: [
             { context: '小鱼 --- (R’U2R)U(R’UR)' },
