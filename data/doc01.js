2b16a9e4f49897092028d097b33090aca89a4587
尹栋力 <1096250669@qq.com>
2019-01-04 15:40:22 +0800

    文档+1 命令

--- data/doc01.js
+++ data/doc01.js
@@ -63,6 +63,48 @@ var demo01 = {
 .zip 压缩 zip filename.zip dirname
 .rar 解压 rar x filename.rar
 .rar 压缩 rar a filename.rar dirname
+
+
+
+
+
+ docker 运行mysql 数据库  
+ docker run -p 3306:3306 --name mymysql -v $PWD/conf:/etc/mysql/conf.d -v $PWD/logs:/logs -v $PWD/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.6
+
+-p 3306:3306：将容器的 3306 端口映射到主机的 3306 端口。
+-v -v $PWD/conf:/etc/mysql/conf.d：将主机当前目录下的 conf/my.cnf 挂载到容器的 /etc/mysql/my.cnf。
+-v $PWD/logs:/logs：将主机当前目录下的 logs 目录挂载到容器的 /logs。
+-v $PWD/data:/var/lib/mysql ：将主机当前目录下的data目录挂载到容器的 /var/lib/mysql 。
+-e MYSQL_ROOT_PASSWORD=123456：初始化 root 用户的密码。
+
+
+
+docker
+
+删除所有已经停止的容器 docker rm $(docker ps -a -q)
+杀死所有正在运行的容器
+docker kill $(docker ps -a -q)
+
+删除所有已经停止的容器
+docker rm $(docker ps -a -q)
+
+ 删除所有未打 dangling 标签的镜像
+docker rmi $(docker images -q -f dangling=true)
+
+删除所有镜像
+docker rmi $(docker images -q)
+
+强制删除镜像名称中包含“doss-api”的镜像
+docker rmi --force $(docker images | grep doss-api | awk '{print $3}')
+
+删除所有未使用数据
+docker system prune
+
+只删除未使用的volumes
+docker volume prune
+
+删除镜像
+docker rmi [imageId]
  */           
 
             /**
