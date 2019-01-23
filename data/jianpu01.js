4bf784990cfae73f5a9aa6e5822f92af154bb35e
尹栋力 <1096250669@qq.com>
2019-01-23 15:12:16 +0800

    简谱+1

--- data/jianpu01.js
+++ data/jianpu01.js
@@ -153,6 +153,7 @@ var demo01 = {
             { context: '<a href="' + urlposition + '清明雨上.jpg">清明雨上</a>' },
             { context: '<a href="' + urlposition + '晴空月儿明.jpg">晴空月儿明</a>' },
             { context: '<a href="' + urlposition + '千古绝唱.jpg">千古绝唱</a>' },
+            { context: '<a href="' + urlposition + '千年风雅.jpg">千年风雅</a>' },
         ]
     }, {
         title: "R开头的简谱",
