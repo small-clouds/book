ed8fcdbda8ef3bf917f51a116d77ac1d6a35bb4d
尹栋力 <1096250669@qq.com>
2019-03-15 17:38:07 +0800

    修改bug

---
+++ data/doc/article/ZiZhiTongJian.js
@@ -0,0 +1,41 @@
+var demo01 = {
+    title: "资治通鉴",
+    name: "资治通鉴",
+    data: [{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },{
+        title: "周纪·周纪一",
+        data: [
+            { context: " " },
+        ]
+    },]
+}
+data = [demo01]
