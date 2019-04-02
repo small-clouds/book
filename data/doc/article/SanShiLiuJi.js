ebe299dde284fb27ca835d66319ec6d72a83573d
尹栋力 <1096250669@qq.com>
2019-04-02 17:50:25 +0800

    很多

---
+++ data/doc/article/SanShiLiuJi.js
@@ -0,0 +1,175 @@
+var demo = {
+    title: "三十六计",
+    name: "三十六计",
+    data: [{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：备周则意怠；常见则不疑。阴在阳之内，不在阳之对。太阳，太阴③。" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·围魏救赵",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },{
+        title: "三十六计·胜战计·瞒天过海",
+        data: [
+            { context: "【原典】：" },
+            { context: "【按语】：" },
+        ]
+    },
+]
+}
+data = [demo]
