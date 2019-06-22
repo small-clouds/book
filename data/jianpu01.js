65adb1071ba0cacfca635ba7655774da1a3f07cf
15030545985 <1096250669@qq.com>
2019-06-22 08:08:02 +0800

    简谱+1

--- data/jianpu01.js
+++ data/jianpu01.js
@@ -50,6 +50,7 @@ var demo01 = {
             { context: '<a href="' + urlposition + '大约在冬季.jpg">大约在冬季</a>' },
             { context: '<a href="' + urlposition + '东风志.jpg">东风志</a>' },
             { context: '<a href="' + urlposition + '刀剑如梦.jpg">刀剑如梦</a>' },
+            { context: '<a href="' + urlposition + '杜十娘.jpg">杜十娘</a>' },
         ]
     }, {
         title: "F开头的简谱",
