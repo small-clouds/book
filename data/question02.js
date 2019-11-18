061a51f5a2f4c2a12e0c04a3c1b4f3bc2293e855
unknown <1096250669@qq.com>
2019-11-18 11:31:38 +0800

    1

--- data/question02.js
+++ data/question02.js
@@ -3,143 +3,147 @@ var demo01 = {
     title: "JAVA面试题--spring",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
+        title: "1、ioc和aop的原理说一下",
         data: [
             { context: "" },
         ]
     }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "2、spring中的过滤器和拦截器有什么区别，你是如何理解的",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+        title: "3、spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring cloud 和dubbo的区别是什么",
+        title: "4、spring cloud 和dubbo的区别是什么",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
+        title: "5、beanFactory 和ApplicationContext有什么区别",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, 
+    }, {
+        title: "6、spring事务的传播机制说一下",
+        data: [
+            { context: "" },
+            { context: "" }
+        ]
+    }, {
+        title: "7、spring 常用的注解有哪些",
+        data: [
+            { context: "" },
+            { context: "" }
+        ]
+    }, {
+        title: "8、spring加载文件的顺序是什么",
+        data: [
+            { context: "" },
+            { context: "" }
+        ]
+    },
     ]
 }
 //春哥总结的面试题目-java
 var demo02 = {
-    title: "JAVA面试题--spring",
+    title: "JAVA面试题--java",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
+        title: "1、java集合有哪些（List；Map；Set）？hashMap和ArrayList的源码是否看过？ConcurentHashMap使用过吗？如果处理并发的。",
         data: [
             { context: "" },
         ]
     }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "2、抽象类和接口的区别是什么？",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+        title: "3、synchronize和lock区别，底层实现原理？",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring cloud 和dubbo的区别是什么",
+        title: "4、设计模式用过哪些，能写一下单例模式的实现吗？",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
+        title: "5、线程池用过吗？如何启动线程，是否看过源码？",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, 
+    }, {
+        title: "6、java锁都有哪些？偏向锁、轻量级锁，重量级锁是什么。公平锁和非公平锁又是什么",
+        data: [
+            { context: "" },
+            { context: "" }
+        ]
+    }
     ]
 }
 //春哥总结的面试题目-并发
 var demo03 = {
-    title: "JAVA面试题--spring",
+    title: "JAVA面试题--并发",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
-        data: [
-            { context: "" },
-        ]
-    }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "1、ThredLocal用过吗？底层实现原理是怎样的",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+        title: "2、CAS知道吗？ABA问题怎么解决的？JUC包下的类都用过哪些",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, {
-        title: "spring cloud 和dubbo的区别是什么",
-        data: [
-            { context: "" },
-            { context: "" }
-        ]
-    }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
-        data: [
-            { context: "" },
-            { context: "" }
-        ]
-    }, 
+    },
     ]
 }
 //春哥总结的面试题目-mybatis
 var demo04 = {
-    title: "JAVA面试题--spring",
+    title: "JAVA面试题--mybatis",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
-        data: [
-            { context: "" },
-        ]
-    }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "1、Mybatis插件有了解过吗",
         data: [
             { context: "" },
             { context: "" }
         ]
     }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+        title: "2、实现原理能说一下嘛",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, {
-        title: "spring cloud 和dubbo的区别是什么",
+    },
+    {
+        title: "3、一级缓存和二级缓存说一下",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
+    },
+    {
+        title: "4、#{}和${} 的区别是什么",
         data: [
             { context: "" },
             { context: "" }
         ]
-    }, 
+    },
     ]
 }
 //春哥总结的面试题目-redis
@@ -147,69 +151,55 @@ var demo05 = {
     title: "JAVA面试题--spring",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
+        title: "1、你们公司用redis是做什么的，基本类型知道多少",
         data: [
             { context: "" },
         ]
-    }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+    },{
+        title: "2、redis的内存淘汰策略都有哪些？",
         data: [
             { context: "" },
-            { context: "" }
         ]
-    }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+    }, 
+    {
+        title: "3、redis持久化机制都有哪些（RDB和AOF）",
         data: [
             { context: "" },
-            { context: "" }
         ]
-    }, {
-        title: "spring cloud 和dubbo的区别是什么",
+    }, 
+    {
+        title: "4、redis主从",
         data: [
             { context: "" },
-            { context: "" }
         ]
-    }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
+    }, 
+    {
+        title: "5、rdis分布式锁怎么实现、",
         data: [
             { context: "" },
-            { context: "" }
+        ]
+    }, 
+    {
+        title: "6、什么是缓存穿透？什么是缓存雪崩？",
+        data: [
+            { context: "" },
         ]
     }, 
     ]
 }
 //春哥总结的面试题目-MQ
 var demo06 = {
-    title: "JAVA面试题--spring",
+    title: "JAVA面试题--MQ",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
+        title: "1、简单说一下MQ的作用",
         data: [
             { context: "" },
         ]
     }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "2、MQ的消息如何顺序之行的",
         data: [
             { context: "" },
-            { context: "" }
-        ]
-    }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
-        data: [
-            { context: "" },
-            { context: "" }
-        ]
-    }, {
-        title: "spring cloud 和dubbo的区别是什么",
-        data: [
-            { context: "" },
-            { context: "" }
-        ]
-    }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
-        data: [
-            { context: "" },
-            { context: "" }
         ]
     }, 
     ]
@@ -219,37 +209,28 @@ var demo07 = {
     title: "JAVA面试题--spring",
     name: "春哥总结的",
     data: [{
-        title: "spring - ioc和aop的原理说一下",
+        title: "1、mysql索引原来说一下",
         data: [
             { context: "" },
         ]
     }, {
-        title: "spring中的过滤器和拦截器有什么区别，你是如何理解的",
+        title: "2、垃圾回收期和垃圾回收机制简单说一下",
         data: [
             { context: "" },
-            { context: "" }
         ]
     }, {
-        title: "spring boot 有什么你使用比较好的特性，或者说为什么使用spring boot",
+        title: "3、OOM都见过哪些",
         data: [
             { context: "" },
-            { context: "" }
         ]
-    }, {
-        title: "spring cloud 和dubbo的区别是什么",
+    },  {
+        title: "4、虚拟机参数调整过什么",
         data: [
             { context: "" },
-            { context: "" }
-        ]
-    }, {
-        title: "beanFactory 和ApplicationContext有什么区别",
-        data: [
-            { context: "" },
-            { context: "" }
         ]
     }, 
     ]
 }
 
 
-data = [demo01,demo02,demo03,demo04,demo05,demo06.demo07];
+data = [demo01, demo02, demo03, demo04, demo05, demo06.demo07];
