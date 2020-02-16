7dbb3ab81da807df75ca414790e10641f09d7d40
1096250669@qq.com <1o96250669@qq.com>
2020-02-16 12:36:35 +0800

    修改bug

---
+++ frame/wordpress/项目需求.js
@@ -0,0 +1,33 @@
+//春普个人网站需求
+ 
+/**
+ * 20200203
+ * 1.微信端单点登录
+ * 2.首页banner
+ * 3.-- 所有课程目录   
+ *      --课程详情目录
+ *          --课程详情
+ *    --- 解决方案  ：：文章目录 存储课程地址 播放页面跳转到新的课程详情页面播放  播放地址为新的单独的  目录可以看到所有的课程  权限判断在新的页面上做
+ * 4.文章权限
+ * 5.用户权限测试
+ * 6.支付功能-购买文章测试 权限测试
+ * 7.订单系统 
+ *        优惠券设置 
+ * 
+ * http://localhost/wordpress/?p=13
+ * 
+
+
+ 
+
+Warning: preg_match(): Compilation failed: nothing to repeat at offset 1 in D:\Program Files (x86)\phpStudy\PHPTutorial\WWW\wordpress\wp-includes\class-wp.php on line 205
+ */
+
+ /**
+全文检索   http://localhost:8085/api/search/all?q=新增&size=5              get
+所有标签   http://localhost:8085/api/learner/qa/home/list          get
+标签分类   http://localhost:8085/api/learner/qa/home/qalabellist          get
+问答数据api   http://localhost:8085/api/learner/qa/home/question            get
+ 修改我的标签  http://localhost:8085/api/learner/qa/home/update?ids=1,2,3       post
+  */
+
