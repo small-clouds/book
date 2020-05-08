09c83a348e232bdf58811a21201f4340b9a29631
尹栋力 <1096250669@qq.com>
2020-05-08 14:16:28 +0800

    11111

--- data/doc/sql/技术整理.sql
+++ data/doc/sql/技术整理.sql
@@ -750,4 +750,196 @@ gradle clean :build -Denv=prod
 ######################################
 
 
+----------------未整理 -------------start------
 
+
+******************  controller页面   ******************************************************************
+/*
+分页
+	@RequestParam(value = "start", defaultValue = "0") int start
+	, @RequestParam(value = "length", defaultValue = AppConstants.PAGE_SIZE) int length
+	
+	PageHelper.startPage(start / length + 1 , length);
+	
+用户权限
+	 and ia.AUDIT_ROLE in (
+              select AUDIT_ROLE from base_user_audit_role where USER_ID=#{userId}
+            )
+			
+list 判空	
+	<if test="list.size() > 0">
+	  _claimreview.id in
+	  <foreach item="item" index="index" collection="list" open="(" separator="," close=")">  
+		#{item}
+	  </foreach>
+	</if>
+ 
+BigDecimal比较大小
+		BigDecimal a = new BigDecimal (101);
+		BigDecimal b = new BigDecimal (111);
+		 
+		//使用compareTo方法比较
+		//注意：a、b均不能为null，否则会报空指针
+
+		if(a.compareTo(b) == -1){
+			System.out.println("a小于b");
+		}
+		 
+		if(a.compareTo(b) == 0){
+			System.out.println("a等于b");
+		}
+		 
+		if(a.compareTo(b) == 1){
+			System.out.println("a大于b");
+		}
+		 
+		if(a.compareTo(b) > -1){
+			System.out.println("a大于等于b");
+		}
+		 
+		if(a.compareTo(b) < 1){
+			System.out.println("a小于等于b");
+		}
+
+*/
+
+
+******************  Html页面   ******************************************************************
+/*
+循环
+	<select id="store" class="form-control">
+		<option value="">全部</option>
+		<option th:each="item:${baseStoreList}" th:value="${item.storeId}"
+				th:text="${item.storeName}">
+		</option>
+	</select>
+	
+验证
+	data-rule-required="true"  data-msg-required="请填写名称"  
+	data-rule-checkCount="true" maxlength="10" 
+	data-rule-number="true" data-msg-number="请填写数字!" 
+	data-rule-min="0" data-msg-min="不能小于0!" 
+	data-rule-rangelength="[8,50]" data-msg-rangelength="新密码长度必须介于8和50之间"
+	
+	data-rule-number="true" data-msg-number="请填写数字!"
+	data-rule-min="0" data-msg-min="不能小于0!"
+
+返回按钮
+	<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal" id="btnBack"><i class="fa fa-reply"></i> 返回</button>
+	//返回
+	$("#btnBack").click(function() {
+		history.go(-1);
+	});
+*/
+
+
+
+
+
+******************  js页面   ******************************************************************
+/*
+弹框提示
+	$.modal.msgSuccess("保存成功");
+	$.modal.alertError("保存失败");
+	
+ajax提交
+	ajaxSubmit(ctx + 'resource/city/batch/submit', 
+	{"provResIds": provResIds.join(",")}
+	, function (data) {
+		if (data.ok) {
+			$.modal.msgSuccess("提交成功！");
+			$table.ajax.reload();//刷新表格
+		} else {
+			$.modal.msgError("提交失败！");
+		}
+	})
+确认数量验证
+	 var inputList = $("input.resnumber");
+        if(inputList==null || inputList.length==0){
+            $.modal.msgError("请至少填写一条出库确认单明细信息!") ;
+            return ;
+        }	
+
+*/
+
+
+公司用到的程序	eclispe neno
+	git 
+	vscode
+	node 
+	java
+	gradle
+	angular
+	pxcook
+
+
+命令集合	
+*************         gradle命令集合	
+gradle cleanEcl ecl  	编译命令每次pull代码都应该编译一下
+gradle run 	运行命令
+	
+	
+关于程序要先编译在导入项目	
+	
+	
+Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化构建工具。	
+	
+	
+	
+自动化构建工具	
+	
+	
+	
+	
+	
+spring	
+@Transactional	事务处理
+
+
+
+ *************************   angular(npm)	
+
+npm install 	安装命令(安装配置文件里面有的)
+npm start 	运行命令
+npm config set registry https://registry.npm.taobao.org     	淘宝换源 
+npm config get registry  	查看换源是否成功
+先ctrl+d在ctrl+c 	就能完全退出
+	
+npm install  console-ui-ng@0.0.68	
+npm install bootstrap@4.0.0	
+ npm install -g @angular/cli@1.2.7	安装脚手架
+ng g c 	创建文件工程
+	
+npm run build	检查本地环境代码是否有错误
+ npm start -- --port=4206	
+	
+	
+	
+npm uninstall -g angular-cli 	
+npm cache clean --force 	
+	
+npm install -g @angular/cli@latest	
+
+
+git	
+git pull	
+git clone	
+	
+git stash	暂存起来
+git pull	与服务器同步
+git statsh pop	暂存于服务器同步
+	
+git reset --hard FETCH_HEAD	FETCH_HEAD表示上一次成功git pull之后形成的commit点。然后git pull.
+	
+$ git reflog -10	
+	
+	
+	
+Apache Spark 是专为大规模数据处理而设计的快速通用的计算引擎
+
+	
+mainTyle必须写	net.parim.spark.SparkApplication	
+
+	
+	
+	
