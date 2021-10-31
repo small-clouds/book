ca312267558ac0e5ebb9d86352dfad52573a321b
yindongli <wangwenjuan@parim.com>
2021-10-31 15:18:25 +0800

    简谱+2

--- data/jianpu02.js
+++ data/jianpu02.js
@@ -88,8 +88,10 @@ var demo01 = {
             { context: '278-<a target="_blank"   href="' + urlposition + '大鱼.jpg">大鱼</a>' },
         ]
     }, {
-        title: "D调的简谱 328",
+        title: "D调的简谱 337",
         data: [
+            { context: '337-<a target="_blank"   href="' + urlposition + '这一拜.jpg">这一拜</a>' },
+            { context: '336-<a target="_blank"   href="' + urlposition + '淯水吟.jpg">淯水吟</a>' },
             { context: '328-<a target="_blank"   href="' + urlposition + '问为何2.jpg">问为何</a>' },
             { context: '327-<a target="_blank"   href="' + urlposition + '问为何1.jpg">问为何</a>' },
             { context: '326-<a target="_blank"   href="' + urlposition + '与君别.jpg">与君别</a>' },
